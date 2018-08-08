a5fdf2d604e2b28640425d872dbfd39b5f66eedf
tangcong <tangcong@ipanel.cn>
2018-08-08 09:06:56 +0800

    完成滚动相关配置—利用better-srcoll库

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="goods">
-        <div class="manu-wrap">
+        <div class="manu-wrap" ref="manuWrap">
             <ul>
-                <li class="manu-list border-1px" v-for="item in goods">
+                <li class="manu-list border-1px"  v-for="(item,index) in goods" :class="{'curr': index === currIndex}" @click="changeIndex(index,$event)">
                     <span class="text">
                        {{item.name}}
                     </span>
                 </li>
             </ul>
         </div>
-        <div class="food-wrap">
+        <div class="food-wrap" ref="foodWrap">
             <ul>
-                <li v-for="item in goods"> 
+                <li v-for="item in goods" class="good-item-hook"> 
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
-                        <li class="good-items border-1px" v-for="good in item.foods">
+                        <li class="good-items border-1px " v-for="good in item.foods">
                             <div class="icon">
                                 <img :src="good.icon">
                             </div>
@@ -40,17 +40,65 @@
 
 <script>
 /* eslint-disable */
+import BScroll from "better-scroll";
 export default {
   data() {
     return {
-      goods: {}
+      goods: {},
+      heightArray: [],
+      scrollY: 0
     };
+  },
+  computed: {
+    currIndex() {
+      for (let i = 0; i < this.heightArray.length; i++) {
+        let height1 = this.heightArray[i];
+        let height2 = this.heightArray[i + 1];
+        let y = this.scrollY;
+        if (y >= height1 && y < height2) {
+          return i;
+        }
+      }
+      return 0;
+    }
   },
   created() {
     this.$http.get("/api/goods").then(res => {
       this.goods = res.body.data;
       console.log(this.goods);
+      this.$nextTick(() => {
+        this._initScroll();
+        this.heightArray.push(0);
+        let height = 0;
+        let dom = document.getElementsByClassName("good-item-hook");
+        for (let i = 0; i < dom.length; i++) {
+          height += dom[i].clientHeight;
+          this.heightArray.push(height);
+        }
+        console.log(this.heightArray);
+      });
     });
+  },
+  methods: {
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.manuWrap, {
+        click: true
+      });
+      this.foodScroll = new BScroll(this.$refs.foodWrap, {
+        click: true,
+        probeType: 3
+      });
+      this.foodScroll.on("scroll", pos => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+    },
+    changeIndex(index, e) {
+      if (!e._constructed) {
+        return;
+      }
+      let el = document.getElementsByClassName('good-item-hook')[index]
+      this.foodScroll.scrollToElement(el, 300);
+    }
   }
 };
 </script>
@@ -73,6 +121,7 @@ export default {
 
         .manu-list {
             height: 54px;
+            width: 56px;
             padding: 0 12px;
             display: table;
             border-1px(rgba(1, 17, 27, 0.1));
@@ -85,6 +134,11 @@ export default {
                 color: rgb(7, 17, 27);
                 vertical-align: middle;
             }
+        }
+
+        .curr {
+            color: rgb(240, 20, 20);
+            background-color: white;
         }
     }
 
@@ -102,43 +156,50 @@ export default {
         }
 
         .good-items {
-            padding 18px
-            display flex
+            padding: 18px;
+            display: flex;
             border-1px(rgba(1, 17, 27, 0.1));
+
             .icon {
-                flex 0 0 57px
-                width 57px
+                flex: 0 0 57px;
+                width: 57px;
+
                 img {
                     width: 57px;
                     height: 57px;
                 }
             }
-            .content{
-                flex 1
-                margin-left 5px
-                .name{
-                    margin-top 2px
-                    font-size 14px
-                    color rgb(7,17,27)
-                    line-height 14px
+
+            .content {
+                flex: 1;
+                margin-left: 5px;
+
+                .name {
+                    margin-top: 2px;
+                    font-size: 14px;
+                    color: rgb(7, 17, 27);
+                    line-height: 14px;
                 }
-                .desc,.extra{
-                    font-size 10px
-                    color rgb(147,153,159)
-                    margin 8px 0 0 0
+
+                .desc, .extra {
+                    font-size: 10px;
+                    color: rgb(147, 153, 159);
+                    margin: 8px 0 0 0;
                 }
-                .price{
-                    .now{
-                        color red
-                        font 10px
-                        line-height 24px
-                        font-weight 700
+
+                .price {
+                    .now {
+                        color: red;
+                        font: 10px;
+                        line-height: 24px;
+                        font-weight: 700;
                     }
-                    .old{
-                        font-size 10px
-                        color rgb(147,153,159)
-                        font-weight 700
-                        text-decoration line-through
+
+                    .old {
+                        font-size: 10px;
+                        color: rgb(147, 153, 159);
+                        font-weight: 700;
+                        text-decoration: line-through;
                     }
                 }
             }
